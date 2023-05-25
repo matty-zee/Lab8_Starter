f06728511e7f475ce048dc0817870f3c4d36dc02
matty-zee <mzheng1108@gmail.com>
2023-05-24 18:47:15 -0700

    finishing part 2

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Lab 8 - Starter
 
 1. You would include your unit tests in a Github action that runs whenever you push your code. That way, testing is completely automated and you won't build something that is broken. 
-2. No. Not directly at least. E2E testing is intended to emulate actions a user might do with your web application to see if it works correctly. This doesn't really directly test the output of a specific function but it can direct you to find a faulty function. 
+2. No. E2E testing is intended to emulate actions a user might do with your web application to see if it works correctly, not individual function correctness. 
+3. No. A messaging feature of an application would definitely have many moving parts involving storing, sending, and receiving messages. Unit tests are intended to test invidvual function usage. 
+4. Yes. This something like a max message length would be detected via a single function that just observes how long a user's message is. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,91 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+//isPhoneNumber Tests
+test('phoneTest 1', () => {
+    expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+})
+
+test('phoneTest 2', () => {
+    expect(functions.isPhoneNumber('(123)456-7890')).toBe(true);
+})
+
+test('phoneTest 3', () => {
+    expect(functions.isPhoneNumber('1234567')).toBe(false);
+})
+
+test('phoneTest 4', () => {
+    expect(functions.isPhoneNumber('1234567890')).toBe(false);
+})
+
+//isEmail tests
+
+test('emailTest 1', () => {
+    expect(functions.isEmail('aaa@gmail.com')).toBe(true);
+})
+
+test('emailTest 2', () => {
+    expect(functions.isEmail('123@gmail.com')).toBe(true);
+})
+
+test('emailTest 3', () => {
+    expect(functions.isEmail('aaa@gmailcom')).toBe(false);
+})
+
+test('emailTest 4', () => {
+    expect(functions.isEmail('aaa@gmail')).toBe(false);
+})
+
+//isStrongPassword tets
+
+test('strongPasswordTest 1', () => {
+    expect(functions.isStrongPassword('abcdefg')).toBe(true);
+})
+
+test('strongPasswordTest 2', () => {
+    expect(functions.isStrongPassword('abcdefg_12')).toBe(true);
+})
+
+test('strongPasswordTest 3', () => {
+    expect(functions.isStrongPassword('1abcdefg')).toBe(false);
+})
+
+test('strongPasswordTest 4', () => {
+    expect(functions.isStrongPassword('abcdefg@')).toBe(false);
+})
+
+//isDate tests
+
+test('isDateTest 1', () => {
+    expect(functions.isDate('11/08/2001')).toBe(true);
+})
+
+test('isDateTest 2', () => {
+    expect(functions.isDate('11/8/2001')).toBe(true);
+})
+
+test('isDateTest 3', () => {
+    expect(functions.isDate('11/08/01')).toBe(false);
+})
+
+test('isDateTest 1', () => {
+    expect(functions.isDate('11/008/2001')).toBe(false);
+})
+
+//isHexColorTests
+
+test('isHexTest 1', () => {
+    expect(functions.isHexColor('#ff00cc')).toBe(true);
+})
+
+test('isHexTest 2', () => {
+    expect(functions.isHexColor('#f0c')).toBe(true);
+})
+
+test('isHexTest 1', () => {
+    expect(functions.isHexColor('#fff00cc')).toBe(false);
+})
+
+test('isHexTest 2', () => {
+    expect(functions.isHexColor('#f00c')).toBe(false);
+})
